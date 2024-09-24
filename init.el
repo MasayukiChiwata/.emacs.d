3bf739d8a5cf508556004fdce9bc25a4df59beb0
chawata-wsl <cha39881@gmail.com>
2024-09-24 23:19:38 +0900

    first commit

---
+++ init.el
@@ -0,0 +1,23 @@
+;;; package --- Summary
+;;; Commentary:
+;;; Code:
+    
+;; パッケージマネージャーのインストール
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '((emacs-lisp . t)))
+
+(org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
